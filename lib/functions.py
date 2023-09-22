15cbf57184daa0ef2076fb41e7fd00441b9a2bf7
Eric Song <ericksong91@gmail.com>
2023-09-22 13:08:50 -0400

    Done with assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
+def my_function(param):
+    print("Running my_function")
+    return param + 1
+
+my_return_function_value = my_function(1)
+
+print(my_return_function_value)
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    print(type(number))
+    if type(number) == "int":
+        return None
+    
+    return number / 2
